3d35e3bb00371fb22b4dd67ecadc540e9ba0bf82
tyfah <tafadzwaallentyfah@gmail.com>
2021-07-25 20:48:57 +0200

    Added creating Test

--- src/test/java/zw/co/afrosoft/junittest/JunitTestApplicationTests.java
+++ src/test/java/zw/co/afrosoft/junittest/JunitTestApplicationTests.java
@@ -1,13 +1,34 @@
 package zw.co.afrosoft.junittest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import zw.co.afrosoft.junittest.model.Product;
+import zw.co.afrosoft.junittest.persistence.ProductRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class JunitTestApplicationTests {
-
+    @Autowired
+    ProductRepository repo;
     @Test
-    void contextLoads() {
+    public void testCreate(){
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Sony Laptop");
+        product.setPrice(500.00);
+        product.setDecription("Brand New Laptop");
+        repo.save(product);
+        assertNotNull(repo.findById(1L).get());
+    }
+    @Test
+    public void testReadAll(){
+        List<Product> list = repo.findAll();
+        assertThat(list).size().isGreaterThan(0);
     }
 
 }
