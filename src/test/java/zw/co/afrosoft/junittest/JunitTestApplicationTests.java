f3e9ebc09287ccb5c9f6a1db96dfc7fd90c46159
tyfah <tafadzwaallentyfah@gmail.com>
2021-07-25 21:36:41 +0200

    Added the rest of the Test cases

--- src/test/java/zw/co/afrosoft/junittest/JunitTestApplicationTests.java
+++ src/test/java/zw/co/afrosoft/junittest/JunitTestApplicationTests.java
@@ -1,6 +1,9 @@
 package zw.co.afrosoft.junittest;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import zw.co.afrosoft.junittest.model.Product;
@@ -9,13 +12,15 @@ import zw.co.afrosoft.junittest.persistence.ProductRepository;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class JunitTestApplicationTests {
     @Autowired
     ProductRepository repo;
     @Test
+    @Order(1)
     public void testCreate(){
         Product product = new Product();
         product.setId(1L);
@@ -26,9 +31,30 @@ class JunitTestApplicationTests {
         assertNotNull(repo.findById(1L).get());
     }
     @Test
+    @Order(2)
     public void testReadAll(){
         List<Product> list = repo.findAll();
         assertThat(list).size().isGreaterThan(0);
+    }
+    @Test
+    @Order(3)
+    public void testSingleProduct(){
+        Product product = repo.findById(1L).get();
+        assertEquals(500.00,product.getPrice());
+    }
+    @Test
+    @Order(4)
+    public void testUpdate(){
+        Product product = repo.findById(1L).get();
+        product.setPrice(200.00);
+        repo.save(product);
+        assertNotEquals(500.00, repo.findById(1L).get().getPrice());
+    }
+    @Test
+    @Order(5)
+    public void testDelete(){
+        repo.deleteById(1L);
+        assertThat(repo.existsById(1L)).isFalse();
     }
 
 }
